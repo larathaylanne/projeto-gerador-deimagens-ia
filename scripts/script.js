de16b8386570c8e510987c55ddd62936c8df30a0
Lara Thaylanne <larathaylanne@gmail.com>
2025-06-06 21:10:10 -0300

    Subindo o projeto

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+document.getElementById('formgerador')
+.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const textoDigitado = document.getElementById('descrition-text').value;
+    console.log("Texto capturado: ", textoDigitado)
+
+    document.querySelector('.loader').style.display = 'block';
+
+    setTimeout(function() {
+        document.querySelector('.loader').style.display = 'none';
+    
+    console.log("Pronto! Aqui viria a imagem gerada!");
+    }, 3000);
+
+    document.querySelector('.loader p').textContent = "Imagem gerada com sucesso! Redirecionando...";
+
+    setTimeout(function() {
+        localStorage.setItem('descricaoImagem', textoDigitado);
+
+        window.location.href = "galeria.html";
+    }, 2000);
+
+}, 3000);
